d51ff30d8cebf4d8d5c90428eedbc19f8b9d5f9d
Fang Georgina <fgeorgin@at-l4.21-school.ru>
2019-05-19 20:10:30 +0300

    7

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,19 +6,19 @@
 /*   By: fgeorgin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/03 21:56:17 by fgeorgin          #+#    #+#             */
-/*   Updated: 2019/05/17 19:38:59 by fgeorgin         ###   ########.fr       */
+/*   Updated: 2019/05/19 20:06:21 by fgeorgin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 8575
 # define LIM 9999
 # include <stdlib.h>
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
-# include "libft/libft.h"
+# include "./libft/libft.h"
 
 int get_next_line(const int fd, char **line);
 int ft_gnl(char **s, char **line);
